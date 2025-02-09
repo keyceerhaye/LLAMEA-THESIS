287b1c6c5e837e18e4623791873e5a46d86deb13
Haoran Yin <h.yin@liacs.leidenuniv.nl>
2025-02-10 00:02:54 +0100

    auc results for exp with description and insights

--- exp_scripts/CAI/post_CAI.py
+++ exp_scripts/CAI/post_CAI.py
@@ -29,41 +29,52 @@ def extract_auc(exp_folder):
     return aucs, final_y
 
 
-exps = ["Photonic01", "Photonic02", "Photonic03"]
-labels = ["Bragg Mirror", "Ellipsometry", "Photovoltaics"]
+exps = ["bragg",
+        "bragg_with_description",
+        "bragg_with_description_insight",
+        "ellipsometry",
+        "ellipsometry_with_description",
+        "ellipsometry_with_description_insight"]
+labels = ["mini-bragg",
+          "mini-bragg with description",
+          "mini-bragg with description\nand algorithmic insights",
+          "ellipsometry",
+          "ellipsometry with description",
+          "ellipsometry with description\nand algorithmic insights"]
 cud = ["#e69f00", "#56b4e9", "#009e73", "#f0e442",
        "#0072b2", "#d55e00", "#cc79a7", "#000000"]
-# auc_data = []
-# y_data = []
-# exp_folders = os.listdir(f"exp_data/CAI/clip/")
-# for exp in exps:
-#     aucs = []
-#     ys = []
-#     for exp_folder in exp_folders:
-#         if exp not in exp_folder:
-#             continue
-#         auc, y = extract_auc(f"exp_data/CAI/clip/{exp_folder}")
-#         if auc == None:
-#             print(f"Skipping {exp_folder}")
-#             continue
-#         aucs += [auc]
-#         ys += [y]
-#     max_len = max([len(auc) for auc in aucs])
-#     aucs = [auc + [auc[-1]]*(max_len - len(auc)) for auc in aucs]
-#     auc_data += [np.array(aucs)]
-#     y_data += [np.array(ys)]
-# current_best_aucs = [np.maximum.accumulate(aucs, axis=1) for aucs in auc_data]
-# keys = ["generation", "auc", "final_y", "run", "problem"]
-# values = []
-# for k in range(len(current_best_aucs)):
-#     array = current_best_aucs[k]
-#     y = y_data[k]
-#     for i in range(5):
-#         for j in range(100):
-#             values += [[j, array[i, j], y[i, j], i, labels[k]]]
-# df = pd.DataFrame(values, columns=keys)
-# df.to_csv("exp_data/CAI/real/conv_plot.csv")
-df = pd.read_csv("exp_data/CAI/real/conv_plot.csv")
+auc_data = []
+y_data = []
+exp_folders = os.listdir(f"exp_data/CAI/clip/")
+for exp in exps:
+    aucs = []
+    ys = []
+    for exp_folder in exp_folders:
+        contents = exp_folder.split("-")
+        if contents[-1] != exp:
+            continue
+        auc, y = extract_auc(f"exp_data/CAI/clip/{exp_folder}")
+        if auc == None or len(auc) < 100:
+            print(f"Skipping {exp_folder}")
+            continue
+        aucs += [auc]
+        ys += [y]
+    max_len = max([len(auc) for auc in aucs])
+    aucs = [auc + [auc[-1]]*(max_len - len(auc)) for auc in aucs]
+    auc_data += [np.array(aucs)]
+    y_data += [np.array(ys)]
+current_best_aucs = [np.maximum.accumulate(aucs, axis=1) for aucs in auc_data]
+keys = ["generation", "auc", "final_y", "run", "problem"]
+values = []
+for k in range(len(current_best_aucs)):
+    array = current_best_aucs[k]
+    y = y_data[k]
+    for i in range(len(y)):
+        for j in range(100):
+            values += [[j, array[i, j], y[i, j], i, labels[k]]]
+df = pd.DataFrame(values, columns=keys)
+df.to_csv("exp_data/CAI/real/conv_plot_description_insight.csv")
+df = pd.read_csv("exp_data/CAI/real/conv_plot_description_insight.csv")
 for i, label in enumerate(labels):
     df_label = df[df["problem"] == label]
     sns.lineplot(x="generation", y="auc", data=df_label,
@@ -71,7 +82,7 @@ for i, label in enumerate(labels):
 # plt.yscale("log")
 plt.ylabel("AOCC")
 plt.legend()
-plt.savefig("results/CAI/real.png")
+plt.savefig("results/CAI/auc_description_insight.png")
 plt.cla()
 
 for i, label in enumerate(labels):
@@ -81,5 +92,5 @@ for i, label in enumerate(labels):
 # plt.yscale("log")
 plt.ylabel(r"$y^*$")
 plt.legend()
-plt.savefig("results/CAI/real_y.png")
+plt.savefig("results/CAI/y_description_insight.png")
 plt.cla()
